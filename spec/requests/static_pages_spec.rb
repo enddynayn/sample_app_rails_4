3c18c3e78cf9352cfaac8e2e7a284c029cbfb0bd
Enddy Dumbrique <enddynayn@hotmail.com>
2013-08-03 15:59:03 -0700

    Implement TDD for about page

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,37 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+  describe "Home page" do
+    before {visit '/static_pages/home'}
+
+    it "should have the content 'Sample App'" do
+    	expect(page).to have_content('Sample App')
+    end
+    it "should have the title 'Home'" do
+      expect(page).to have_title('Ruby on Rails Tutorial Sample App | Home')
+    end
+  end
+
+  describe "Help page" do
+    before {visit '/static_pages/help'}
+
+  	it "should have the content 'Help'" do
+  		expect(page).to have_content('Help')
+  	end
+    it "should have the title 'Help'" do
+      expect(page).to have_title('Ruby on Rails Tutorial Sample App | Help')
+    end
+  end
+  
+  describe "About page" do
+    before {visit '/static_pages/about'}
+
+  	it "should have the content 'About" do
+  		expect(page).to have_content('About')
+  		end
+    it "should have the title 'About'" do
+      expect(page).to have_title('Ruby on Rails Tutorial Sample App | About Us')
+    end
+  end
+
+end
